530abc18d9ce129090973c5aaa976b605a683819
BT_1N3_19@bktech.local <BT_1N3_19@bktech.local>
2018-12-10 11:43:01 -0500

    Initial commit

---
+++ src/DarrenYau/Card.java
@@ -0,0 +1,29 @@
+package DarrenYau;
+
+public class Card {
+    private String rank;
+    private String suit;
+    private int pointValue;
+
+    public Card (String rank, String suit, int pointValue) {
+        this.rank = rank;
+        this.suit = suit;
+        this.pointValue = pointValue;
+    }
+
+    public String getRank(){
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
+
+    public boolean equals(Card otherCard) {
+        
+    }
+}
---
+++ src/DarrenYau/Runner.java
@@ -0,0 +1,8 @@
+package DarrenYau;
+
+public class Runner {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
