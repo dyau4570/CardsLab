6738ef14db823fefa207ef5d90c601566d555fa2
BT_1N3_06 <BT_1N3_06>
2018-12-12 11:35:39 -0500

    class

--- src/DarrenYau/Deck.java
+++ src/DarrenYau/Deck.java
@@ -3,10 +3,60 @@ package DarrenYau;
 import java.util.ArrayList;
 
 public class Deck {
-    private ArrayList<Card> unDealt;
-    private ArrayList<Card> Dealt;
+    ArrayList<Card> unDealt;
+    ArrayList<Card> Dealt;
 
     public Deck(String[] rank, String[] suit, int[] pointValue){
+        Dealt = new ArrayList<Card>();
+        unDealt = new ArrayList<Card>();
+        for (int i = 0; i < suit.length; i++) {
+            for (int j = 0; j < rank.length; j++) {
+                Card newCard = new Card(rank[j], suit[i], pointValue[j]);
+                unDealt.add(newCard);
+            }
+        }
+    }
 
+    public ArrayList<Card> getDealt() {
+        return Dealt;
+    }
+
+    public ArrayList<Card> getUnDealt() {
+        return unDealt;
+    }
+
+    public boolean isEmpty() {
+        if(unDealt.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return unDealt.size();
+    }
+
+    public Card deal() {
+        if(unDealt.size() > 0) {
+            Card temp = unDealt.get(0);
+            Dealt.add(temp);
+            unDealt.remove(0);
+            return temp;
+        }
+        return null;
+    }
+
+
+    public void shuffle() {
+        unDealt.addAll(Dealt);
+        while(Dealt.size()>0) {
+            Dealt.remove(0);
+        }
+        for(int i = unDealt.size()-1; i > 0; i--) {
+            int pos = (int)(Math.random() * i);
+            Card temp = unDealt.get(i);
+            unDealt.set(i, unDealt.get(i));
+            unDealt.set(pos, temp);
+        }
     }
 }
--- src/DarrenYau/DeckTester.java
+++ src/DarrenYau/DeckTester.java
@@ -1,8 +1,16 @@
 package DarrenYau;
 
 public class DeckTester {
-
     public static void main(String[] args) {
-	// write your code here
+        String[] suits = {"Diamonds","Clubs","Hearts","Spades"};
+        String[] ranks = {"1","2","3","4"};
+        int[] pointValues = {1,2,3,4};
+        Deck deck = new Deck(suits,ranks,pointValues);
+        System.out.println(deck.getUnDealt());
+        System.out.println(deck.getDealt());
+        deck.shuffle();
+        while(deck.size() > 0) {
+            System.out.println(deck.deal());
+        }
     }
 }
